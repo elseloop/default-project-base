530a21e83c93757626a0a158803a5252ecc80ae0
Dan Manchester <dan@elseloop.com>
2014-01-03 11:58:38 -0800

    Swap pseudo element for span on features to enable click event

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 (function($){
 
+  // expand the phone image on :hover
   $( '.teaser--img' ).hover( function(){
       $(this).addClass('expand');
     },
@@ -7,5 +8,11 @@
       $(this).removeClass('expand');
     }
   );
+
+  // add a click event to expand the phone image, since the icon's there...
+  $( '.expando' ).on( 'click', function(){
+    // $(this).css('top', '12px');
+    $(this).closest('.teaser-block').find('.teaser--img').toggleClass('expand');
+  });
 
 })(jQuery);
